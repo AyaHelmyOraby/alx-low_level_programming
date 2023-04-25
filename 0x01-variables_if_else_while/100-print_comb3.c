8fbcfe9458ca23eb2317452dae4f1c83da77b779
AyaHelmyOraby <araby8583@gmail.com>
2023-04-25 01:53:06 -0700

    philomath

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all possible different
+ *          combination of two digits.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit1 = 0, digit2;
+
+	while (digit2 <= 9)
+	{
+		digit2 = 0;
+
+		while (digit2 <= 9 && digit1 < digit2)
+		{
+			if (digit1 != digit2 && digit1 < digit2)
+			{
+				putchar(digit1 + 48);
+				putchar(digit2 + 48);
+
+				if (digit1 + digit2 !=  17)
+				{
+					putchar(',');
+					purchar(' ');
+				}
+			}
+			digit2++;
+		}
+		digit1++;
+	}
+	purchar('\n');
+
+	return (0);
+}
+
+
+
