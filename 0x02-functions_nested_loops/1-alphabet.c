5bf840768b33889c99cd1982eef0f147dd400d94
AyaHelmyOraby <araby8583@gmail.com>
2023-04-27 18:04:54 -0700

    done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,17 @@
 #include"main.h"
-
 /**
- * print_alphapet - utilizes on the _putchar function to print
- *                 the alphapet a -z
+ * print_alphapet - utilizes on the _putchar function to pr
+ i*
+ * Return: Always 0 (Success)
 */
 
 void print_alphaprt(void)
 {
-	int ch;
+	int i;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-		_putchar(ch);
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
 	_putchar('\n');
 }
