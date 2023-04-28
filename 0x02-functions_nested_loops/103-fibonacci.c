b4c1c7c3da6b06cf641d3941b5458332f45065b3
AyaHelmyOraby <araby8583@gmail.com>
2023-04-28 15:58:07 -0700

    done

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -13,20 +13,23 @@ int main(void)
 {
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
+	float total;
 
-	for (count = 0; count < 50; count++)
+	while (1)
 	{
 		sum = fib1 + fib2;
+
+		if (sum > 4000000)
+			break;
+
+		if ((sum % 2) == 0)
+			total += sum;
 		printf("%lu", sum);
 
 		fib1 = fib2;
 		fib2 = sum;
-
-		if (count == 49)
-			printf("\n");
-		else
-			printf(", ");
 	}
+	printf("%.of\n", total);
 
 	return (0);
 }
