1400b3ba5d378b3361a078675aa95276c8dfafa3
AyaHelmyOraby <araby8583@gmail.com>
2023-04-27 18:51:24 -0700

    done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * print_alphapet_x10 -Prints the alphapet 10 times
- *
- * Return: Always 0 (Success)
- */
+* print_alphapet_x10 -Prints the alphapet 10 times
+*
+* Return: Always 0 (Success)
+*/
 void print_alphapet_x10(void)
 {
 	int i;
