6a7ae8e05e71fd23e3e4ac0033d1b99ec86c16e1
AyaHelmyOraby <araby8583@gmail.com>
2023-05-04 18:30:03 -0700

    done

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @s: input string.
+ * Return: the pointer to dest.
+ */
+
+char *rot13(char *s)
+{
+	int i, z;
+
+	char *x = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *y = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (z = 0; x[z] != '\0'; z++)
+		{
+			if (s[i] == x[z])
+			{
+				s[i] = y[z];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
