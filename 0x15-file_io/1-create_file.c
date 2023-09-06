0e94b6ae0fc8562f2a1046dd2aca6cbb99839b36
AyaHelmyOraby <araby8583@gmail.com>
2023-09-06 13:19:48 -0700

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int filedata, write, length = 0;
+	int filedata, write1, length = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	filedata = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	write = write(filedata, text_content, length);
+	write1 = write(filedata, text_content, length);
 
 	if (filedata == -1 || write == -1)
 		return (-1);
